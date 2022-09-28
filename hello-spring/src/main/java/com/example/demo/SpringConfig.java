052fa3e4a8fb675586be85c142247cfdb72c59d0
SuminAnn <annsumin2066@naver.com>
2022-09-28 17:34:35 +0900

    Spring스터디(순수 JDBC 연결 및 다형성, DI)

--- hello-spring/src/main/java/com/example/demo/SpringConfig.java
+++ hello-spring/src/main/java/com/example/demo/SpringConfig.java
@@ -1,14 +1,22 @@
 package com.example.demo;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.example.demo.repository.JdbcMemberRepository;
 import com.example.demo.repository.MemberRepository;
-import com.example.demo.repository.MemoryMemberRepository;
 import com.example.demo.service.MemberService;
 
 @Configuration
 public class SpringConfig {
+	
+	private final DataSource dataSource;
+	
+	public SpringConfig(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
 
 	@Bean
 	public MemberService memberService() {
@@ -17,6 +25,8 @@ public class SpringConfig {
 	
 	@Bean
 	public MemberRepository memberRepository() {
-		return new MemoryMemberRepository();
+//		return new MemoryMemberRepository();
+		return new JdbcMemberRepository(dataSource); // interface 구현체를 바꾸면서 기존 코드를 변경하지 않고 변경할수 있는 점이 객체지향 언어의 장점이다
+		// interface를 두고 구현체를 변경을 통해 디형성을 활용 (spring contatiner가 지원을 해준다)
 	}
 }
